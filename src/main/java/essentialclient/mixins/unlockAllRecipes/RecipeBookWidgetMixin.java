3b309fb3d3d82cf8e202d09d1febb272f08f8058
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-10-31 19:07:24 -0700

    Fix: overlapping ghost recipes

--- src/main/java/essentialclient/mixins/unlockAllRecipes/RecipeBookWidgetMixin.java
+++ src/main/java/essentialclient/mixins/unlockAllRecipes/RecipeBookWidgetMixin.java
@@ -3,10 +3,13 @@ package essentialclient.mixins.unlockAllRecipes;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
 import net.minecraft.client.gui.screen.recipebook.RecipeResultCollection;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeMatcher;
 import net.minecraft.screen.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -23,10 +26,18 @@ public abstract class RecipeBookWidgetMixin {
     @Shadow
     public abstract void showGhostRecipe(Recipe<?> recipe, List<Slot> slots);
 
+    @Unique
+    RecipeMatcher matcher = new RecipeMatcher();
+
     @Inject(method = "mouseClicked", locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "net/minecraft/client/network/ClientPlayerInteractionManager.clickRecipe(ILnet/minecraft/recipe/Recipe;Z)V"))
     private void postClickRecipe(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir, Recipe<?> recipe, RecipeResultCollection recipeResultCollection) {
-        if (this.client.player != null) {
-            this.showGhostRecipe(recipe, this.client.player.currentScreenHandler.slots);
+        PlayerEntity player = this.client.player;
+        if (player != null) {
+            player.inventory.populateRecipeFinder(this.matcher);
+            if (!this.matcher.match(recipe, null)) {
+                this.showGhostRecipe(recipe, this.client.player.currentScreenHandler.slots);
+            }
+            matcher.clear();
         }
     }
 }
