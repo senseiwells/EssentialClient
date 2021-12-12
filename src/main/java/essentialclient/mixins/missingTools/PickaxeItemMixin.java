d01bd624c618232e2daad9e1fcacca5c2e2e49aa
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-12-12 00:42:05 -0800

    refactor: miningSpeeds

--- src/main/java/essentialclient/mixins/missingTools/PickaxeItemMixin.java
+++ src/main/java/essentialclient/mixins/missingTools/PickaxeItemMixin.java
@@ -1,20 +1,28 @@
 package essentialclient.mixins.missingTools;
 
 import essentialclient.config.clientrule.ClientRules;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
 
 @Mixin(PickaxeItem.class)
-public class PickaxeItemMixin extends MiningToolItemMixin {
+public class PickaxeItemMixin extends MiningToolItem {
+
     //Client implementation of missingTools
+    protected PickaxeItemMixin(float attackDamage, float attackSpeed, ToolMaterial material, Set<Block> tag, Item.Settings settings) {
+        super(attackDamage, attackSpeed, material, tag, settings);
+    }
+
     @Override
-    public void miningSpeedHandler(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-        if (ClientRules.MISSING_TOOLS.getValue() && state.getMaterial() == Material.GLASS) {
-            cir.setReturnValue(this.miningSpeed);
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        Material material = state.getMaterial();
+        if (ClientRules.MISSING_TOOLS.getValue() && material == Material.GLASS) {
+            return miningSpeed;
         }
+        return super.getMiningSpeedMultiplier(stack, state);
     }
 }
