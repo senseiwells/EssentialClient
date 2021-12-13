e62292cc6af82b9f53e65aba01f035917d2ac197
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-12-13 01:37:45 -0800

    fix: itemscroller fix fixed too well

--- src/main/java/essentialclient/mixins/unlockAllRecipes/RecipeManagerMixin.java
+++ src/main/java/essentialclient/mixins/unlockAllRecipes/RecipeManagerMixin.java
@@ -1,7 +1,5 @@
 package essentialclient.mixins.unlockAllRecipes;
 
-import essentialclient.config.clientrule.ClientRules;
-import essentialclient.feature.RecipeBookCache;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeManager;
@@ -16,10 +14,11 @@ import java.util.stream.Stream;
 public class RecipeManagerMixin {
     @Redirect(method = "getFirstMatch", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;findFirst()Ljava/util/Optional;"))
     public <C extends Inventory, T extends Recipe<C>> Optional<T> onCall(Stream<T> instance) {
+        // TODO this is bad. Breaks vanilla recipe lookup in single player too. Fix item scroller in some other way
         Optional<T> result = instance.findFirst();
-        if (ClientRules.UNLOCK_ALL_RECIPES_ON_JOIN.getValue() && result.isPresent() && RecipeBookCache.isCached(result.get())) {
-            return Optional.empty();
-        }
-        return result;  
+//        if (ClientRules.UNLOCK_ALL_RECIPES_ON_JOIN.getValue() && result.isPresent() && RecipeBookCache.isCached(result.get())) {
+//            return Optional.empty();
+//        }
+        return result;
     }
 }
