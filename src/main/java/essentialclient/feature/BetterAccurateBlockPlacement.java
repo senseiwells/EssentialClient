ae41d93b2b0f7fd6aa0629ad2f1aa1c7acc068a8
AngelBottomless <35677394+aria1th@users.noreply.github.com>
2022-01-19 20:57:17 +0900

    make BetterAccurateBlockPlacement available in script
    Starting from this (to make branch)

--- src/main/java/essentialclient/feature/BetterAccurateBlockPlacement.java
+++ src/main/java/essentialclient/feature/BetterAccurateBlockPlacement.java
@@ -12,28 +12,54 @@ import net.minecraft.util.math.Direction;
 
 public class BetterAccurateBlockPlacement {
 
-	public static Direction fakeDirection;
-
+	public static Direction fakeDirection = null;
 	public static float fakeYaw = 0;
 	public static float fakePitch = 0;
-
+	public static float fakeRequestYaw = 0;
+	public static float fakeRequestPitch = 0;
+	public static boolean onRequest = false;
+	public static int count = 0;
 	private static boolean wasReversePressed = false;
 	private static boolean wasIntoPressed = false;
 
 	public static void register() {
-		ClientTickEvents.END_CLIENT_TICK.register(BetterAccurateBlockPlacement::accurateBlockPlacement);
+		ClientTickEvents.END_CLIENT_TICK.register(essentialclient.feature.BetterAccurateBlockPlacement::accurateBlockPlacementOnPress);
+		ClientTickEvents.END_CLIENT_TICK.register(essentialclient.feature.BetterAccurateBlockPlacement::accurateBlockPlacementRequest);
 	}
-
-	private static void accurateBlockPlacement(MinecraftClient client) {
-		fakeDirection = null;
+	private static void accurateBlockPlacementRequest(MinecraftClient client){
+		ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
+		ClientPlayerEntity playerEntity = client.player;
+		KeyBinding reverseKeyBinding = ClientKeybinds.ACCURATE_REVERSE.getKeyBinding();
+		KeyBinding intoKeyBinding = ClientKeybinds.ACCURATE_INTO.getKeyBinding();
+		if (playerEntity == null || networkHandler == null) {
+			return;
+		}
+		if (onRequest && count > 0){
+			networkHandler.sendPacket(new PlayerMoveC2SPacket.LookOnly(//1.17 then LookAndOnGround
+				fakeRequestYaw,
+				fakeRequestPitch,
+				playerEntity.isOnGround()
+			));
+			count--;
+		} else {
+			onRequest = false;
+			count = 0;
+		}
+		if (!onRequest && !reverseKeyBinding.isPressed() && !intoKeyBinding.isPressed()){
+			fakeRequestYaw = playerEntity.yaw;
+			fakeRequestPitch = playerEntity.pitch;
+			fakeDirection = null;
+		}
+	}
+	private static void accurateBlockPlacementOnPress(MinecraftClient client) {
 		ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
 		if (ClientRules.BETTER_ACCURATE_BLOCK_PLACEMENT.getValue() && networkHandler != null) {
 			ClientPlayerEntity playerEntity = client.player;
 			if (playerEntity == null) {
 				return;
 			}
-			fakeYaw = playerEntity.yaw;
-			fakePitch = playerEntity.pitch;
+			fakeYaw = playerEntity.yaw; //getYaw();
+			fakePitch = playerEntity.pitch; //getPicth();
 			Direction facing = Direction.getEntityFacingOrder(playerEntity)[0];
 			KeyBinding reverseKeyBinding = ClientKeybinds.ACCURATE_REVERSE.getKeyBinding();
 			KeyBinding intoKeyBinding = ClientKeybinds.ACCURATE_INTO.getKeyBinding();
@@ -41,6 +67,7 @@ public class BetterAccurateBlockPlacement {
 				fakeYaw = 0;
 				fakePitch = 0;
 				facing = blockHitResult.getSide();
+				fakeDirection = facing;
 				switch (facing) {
 					case UP -> fakePitch = -90;
 					case DOWN -> fakePitch = 90;
@@ -68,18 +95,18 @@ public class BetterAccurateBlockPlacement {
 					wasReversePressed = true;
 				}
 				facing = facing.getOpposite();
+				fakeDirection = facing;
 			}
 			else if (wasReversePressed) {
 				sendLookPacket(networkHandler, playerEntity);
 				wasReversePressed = false;
 			}
 			// This is for the client, so it doesn't look jank
-			fakeDirection = facing;
 		}
 	}
 
 	private static void sendLookPacket(ClientPlayNetworkHandler networkHandler, ClientPlayerEntity playerEntity) {
-		networkHandler.sendPacket(new PlayerMoveC2SPacket.LookOnly(
+		networkHandler.sendPacket(new PlayerMoveC2SPacket.LookOnly(//1.17 then LookAndOnGround
 			fakeYaw,
 			fakePitch,
 			playerEntity.isOnGround()
