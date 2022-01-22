cf7782cd29e0594e67c647b6073162fc57871c21
Sensei <wellsm488@gmail.com>
2022-01-22 10:55:25 +0000

    Fix narrator bug

--- src/main/java/essentialclient/mixins/disableNarrator/KeyboardMixin.java
+++ src/main/java/essentialclient/mixins/disableNarrator/KeyboardMixin.java
@@ -11,7 +11,7 @@ public class KeyboardMixin {
 	@ModifyConstant(method = "onKey", constant = @Constant(intValue = 66), require = 0)
 	private int disableNarrator(int old) {
 		if (ClientRules.DISABLE_NARRATOR.getValue()) {
-			return -1;
+			return -2;
 		}
 		return old;
 	}
