1e9a5010b9e6428e58ed9fd86de6f83bf52ca93a
Sensei <wellsm488@gmail.com>
2022-07-21 01:10:40 +0100

    RATK mouse compat

--- src/main/java/me/senseiwells/essentialclient/mixins/mouseScrollRules/MouseMixin.java
+++ src/main/java/me/senseiwells/essentialclient/mixins/mouseScrollRules/MouseMixin.java
@@ -1,11 +1,8 @@
 package me.senseiwells.essentialclient.mixins.mouseScrollRules;
 
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import me.senseiwells.essentialclient.rule.ClientRules;
 import net.minecraft.client.Mouse;
-import net.minecraft.entity.player.PlayerInventory;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
@@ -20,10 +17,5 @@ public class MouseMixin {
 	private float newSensitivityLimit(float originalFloat) {
 		int newSensitivity = ClientRules.INCREASE_SPECTATOR_SCROLL_SENSITIVITY.getValue();
 		return newSensitivity > 0 ? originalFloat * newSensitivity : originalFloat;
-	}
-
-	@WrapWithCondition(method = "onMouseScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;scrollInHotbar(D)V"))
-	private boolean onScrollHotbar(PlayerInventory playerInventory, double scrollAmount) {
-		return !ClientRules.DISABLE_HOTBAR_SCROLLING.getValue();
 	}
 }
