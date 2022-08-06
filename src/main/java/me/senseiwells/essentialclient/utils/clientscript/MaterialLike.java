b84f94eed901afefd3d38ab00566ba540b25fda9
aria1th <35677394+aria1th@users.noreply.github.com>
2022-08-07 02:29:59 +0900

    Update MaterialLike.java

--- src/main/java/me/senseiwells/essentialclient/utils/clientscript/MaterialLike.java
+++ src/main/java/me/senseiwells/essentialclient/utils/clientscript/MaterialLike.java
@@ -1,5 +1,6 @@
 package me.senseiwells.essentialclient.utils.clientscript;
 
+import me.senseiwells.arucas.throwables.BuiltInException;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -17,6 +18,6 @@ public interface MaterialLike {
 		if (item instanceof BlockItem blockItem) {
 			return blockItem.getBlock();
 		}
-		throw new RuntimeException("Material cannot be converted into a block");
+		throw new BuiltInException("Material cannot be converted into a block");
 	}
 }
