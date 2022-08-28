3f0724e735058648ddbe11138b5d8da09f1a6dbc
aria1th <35677394+aria1th@users.noreply.github.com>
2022-08-29 03:59:27 +0900

    fix descriptions

--- src/main/java/me/senseiwells/essentialclient/clientscript/definitions/MinecraftClientDef.java
+++ src/main/java/me/senseiwells/essentialclient/clientscript/definitions/MinecraftClientDef.java
@@ -300,12 +300,12 @@ public class MinecraftClientDef extends PrimitiveDefinition<MinecraftClient> {
 	@FunctionDoc(
 		name = "editSign",
 		desc = {
-			"This allows you to edit sign at certain position with given string(lines), max at 4.",
-			"This function does not check if sign is in position / sign is editable."
+			"This allows you to edit sign at certain position with given string(lines), up to 4 lines.",
+			"This function does not check if sign is editable / is in position."
 		},
 		params = {
 			POS, "position", "the position of sign",
-			STRING, "string", "the string you want to put"
+			STRING, "string...", "the lines for the sign, requires 1 string and up to 4 strings"
 		},
 		examples = "client.editSign(new Pos(0,0,0), '100', '101', 'this is third line', 'last line');"
 	)
