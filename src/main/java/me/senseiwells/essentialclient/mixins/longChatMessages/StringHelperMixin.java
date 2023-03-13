d0227be48054088d3898db7aac2feb1fa93e5629
senseiwells <wellsm488@gmail.com>
2023-03-13 09:04:09 +0000

    Fix #106

--- src/main/java/me/senseiwells/essentialclient/mixins/longChatMessages/StringHelperMixin.java
+++ src/main/java/me/senseiwells/essentialclient/mixins/longChatMessages/StringHelperMixin.java
@@ -1,15 +1,17 @@
 package me.senseiwells.essentialclient.mixins.longChatMessages;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.senseiwells.essentialclient.utils.EssentialUtils;
 import net.minecraft.util.StringHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(StringHelper.class)
 public class StringHelperMixin {
 	//#if MC >= 11901
-	@ModifyConstant(method = "truncateChat", constant = @Constant(intValue = 256))
+	@ModifyExpressionValue(method = "truncateChat", at = @At(value = "CONSTANT", args = "intValue=256"))
 	private static int getMaxLength(int original) {
 		return EssentialUtils.getMaxChatLength(original);
 	}
