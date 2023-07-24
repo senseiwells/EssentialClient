28b5a69aeab1d695f74aca68af41dff6357c9a36
senseiwells <wellsm488@gmail.com>
2023-07-24 14:49:55 +0800

    Fix #115 - Incorrect Chunk Debug statuses

--- src/main/java/me/senseiwells/essentialclient/feature/chunkdebug/ChunkStatus.java
+++ src/main/java/me/senseiwells/essentialclient/feature/chunkdebug/ChunkStatus.java
@@ -11,12 +11,21 @@ public enum ChunkStatus implements Colourable {
 	NOISE(Texts.NOISE, 0x61857F), // Grey-blue
 	SURFACE(Texts.SURFACE, 0x013220), // Pine green
 	CARVERS(Texts.CARVERS, 0x88631F), // Brown
-	LIQUID_CARVERS(Texts.LIQUID_CARVERS, 0x9EEEF5), // Baby blue
+	//#if MC < 12000
+	//$$LIQUID_CARVERS(Texts.LIQUID_CARVERS, 0x9EEEF5), // Baby blue
+	//#endif
 	FEATURES(Texts.FEATURES, 0x3659DE), // Blue
+	//#if MC > 12000
+	INITIALIZE_LIGHT(Texts.TICKET_LIGHT, -1),
+	//#endif
 	LIGHT(Texts.TICKET_LIGHT, -1),
 	SPAWN(Texts.SPAWN, 0xBFFF00), // Lime green
-	HEIGHTMAPS(Texts.HEIGHTMAPS, 0xF8518D), // Pink
+	//#if MC < 12000
+	//##HEIGHTMAPS(Texts.HEIGHTMAPS, 0xF8518D), // Pink
+	//#endif
 	FULL(Texts.FULL, 0x0A18D8); // Dark blue
+
+	private static final int COUNT = ChunkStatus.values().length;
 
 	private final Text prettyName;
 	private final int colour;
@@ -33,7 +42,7 @@ public enum ChunkStatus implements Colourable {
 
 	@Override
 	public int getPriority() {
-		return this.ordinal() > 0 && this.ordinal() < 12 ? 3 : 0;
+		return this.ordinal() > 0 && this.ordinal() < (COUNT - 1) ? 3 : 0;
 	}
 
 	@Override
@@ -42,7 +51,7 @@ public enum ChunkStatus implements Colourable {
 	}
 
 	public static ChunkStatus decodeChunkStatus(int code) {
-		if (code < 1 || code > 12) {
+		if (code < 1 || code >= COUNT) {
 			return EMPTY;
 		}
 		return ChunkStatus.values()[code];
