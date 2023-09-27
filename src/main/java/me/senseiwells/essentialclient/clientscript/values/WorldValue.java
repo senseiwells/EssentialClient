67bae5db5e9b95da79017c856580f408a17ac72d
Spongecade <spongecade.129@gmail.com>
2023-09-27 17:01:44 +0000

    [1.19.x] Update Minecraft Wiki links to new domain

--- src/main/java/me/senseiwells/essentialclient/clientscript/values/WorldValue.java
+++ src/main/java/me/senseiwells/essentialclient/clientscript/values/WorldValue.java
@@ -316,7 +316,7 @@ public class WorldValue extends GenericValue<ClientWorld> {
 			name = "getTimeOfDay",
 			desc = {
 				"This will get the time of day of the world",
-				"info on the time of day [here](https://minecraft.fandom.com/wiki/Daylight_cycle)"
+				"info on the time of day [here](https://minecraft.wiki/w/Daylight_cycle)"
 			},
 			returns = {NUMBER, "the time of day of the world, between 0 and 24000"},
 			example = "world.getTimeOfDay();"
@@ -329,7 +329,7 @@ public class WorldValue extends GenericValue<ClientWorld> {
 			name = "renderParticle",
 			desc = {
 				"This will render a particle in the world, you can find a list of all",
-				"the particle ids [here](https://minecraft.fandom.com/wiki/Java_Edition_data_values#Particles)"
+				"the particle ids [here](https://minecraft.wiki/w/Java_Edition_data_values#Particles)"
 			},
 			params = {
 				STRING, "particleId", "the id of the particle",
@@ -359,7 +359,7 @@ public class WorldValue extends GenericValue<ClientWorld> {
 			name = "renderParticle",
 			desc = {
 				"This will render a particle in the world, you can find a list of all",
-				"the particle ids [here](https://minecraft.fandom.com/wiki/Java_Edition_data_values#Particles)"
+				"the particle ids [here](https://minecraft.wiki/w/Java_Edition_data_values#Particles)"
 			},
 			params = {
 				STRING, "particleId", "the id of the particle",
@@ -393,7 +393,7 @@ public class WorldValue extends GenericValue<ClientWorld> {
 			name = "renderParticle",
 			desc = {
 				"This will render a particle in the world with a velocity, you can find a list of all",
-				"the particle ids [here](https://minecraft.fandom.com/wiki/Java_Edition_data_values#Particles)"
+				"the particle ids [here](https://minecraft.wiki/w/Java_Edition_data_values#Particles)"
 			},
 			params = {
 				STRING, "particleId", "the id of the particle",
