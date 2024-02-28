d8f01b8a49bde3a3bdd27667d5557ab3bac005f4
senseiwells <wellsm488@gmail.com>
2024-02-28 09:43:00 +0000

    Fix #129

--- src/main/java/me/senseiwells/essentialclient/mixins/keyboard/KeyboardMixin.java
+++ src/main/java/me/senseiwells/essentialclient/mixins/keyboard/KeyboardMixin.java
@@ -6,9 +6,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(Keyboard.class)
+@Mixin(value = Keyboard.class, priority = 1100)
 public class KeyboardMixin {
-	@ModifyConstant(method = "onKey", constant = @Constant(intValue = 292))
+	@ModifyConstant(method = "onKey", constant = @Constant(intValue = 292), require = 0)
 	private int onKey292(int original) {
 		return ClientKeyBinds.TOGGLE_DEBUG_MENU.getBoundKey().getCode();
 	}
